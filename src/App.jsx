16bd8c80d7be8bf72eb8e4ab4b5017abd8b9f807
Sharukh Khan <enetsharukh@gmail.com>
2025-10-27 22:15:57 +0530

    adding pages for diffrengt component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React,{useState} from 'react'
-import { useLocation } from 'react-router-dom'
+import { Routes, Route,useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import CarDetail from './pages/CarDetail'
+import Cars from './pages/Cars'
+import Home from './pages/Home'
+import MyBooking from './pages/MyBooking'
 
 const App = () => {
   const [showLogin, setShowLogin]= useState(false)
@@ -8,6 +12,12 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath &&< Navbar setShowLogin={setShowLogin}/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetail/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBooking/>}/>
+      </Routes>
     </div>
   )
 }
