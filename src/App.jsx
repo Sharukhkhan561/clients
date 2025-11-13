b9a99f1062f6a9b2fad4e900dcc557711a633218
Sharukh Khan <enetsharukh@gmail.com>
2025-11-13 18:51:17 +0530

    Adding Sidebar component.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import CarDetail from './pages/CarDetail'
 import Cars from './pages/Cars'
 import Home from './pages/Home'
 import MyBooking from './pages/MyBooking'
+import Layout from './pages/owner/Layout'
+import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
+
 
 const App = () => {
   const [showLogin, setShowLogin]= useState(false)
@@ -18,6 +24,13 @@ const App = () => {
         <Route path='/car-details/:id' element={<CarDetail/>}/>
         <Route path='/cars' element={<Cars/>}/>
         <Route path='/my-bookings' element={<MyBooking/>}/>
+        <Route path='/owner' element={<Layout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='add-car' element={<AddCar/>}/>
+        <Route path='manage-cars' element={<ManageCars/>}/>
+        <Route path='manage-bookings' element={<ManageBookings/>}/>
+
+        </Route>
       </Routes>
 
       {!isOwnerPath && <Footer/>}
---
+++ src/pages/owner/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavbarOwner from '../../components/owner/NavbarOwner'
+import SideBar from '../../components/owner/SideBar'
+
+const Layout = () => {
+  return (
+    <div className='flex flex-col'>
+        <NavbarOwner/>
+        <div className='flex'>
+            <SideBar/>
+            <Outlet/>
+        </div>
+    </div>
+  )
+}
+
+export default Layout
